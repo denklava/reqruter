fd917f42925394c3ef7a2f984f18ba73e41befe9
Daliborka Josipovic <daliborka.josipovic@gmail.com>
2014-09-16 00:20:44 +0200

    Initial code structure

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name reqruterApp
+ * @description
+ * # reqruterApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('reqruterApp', [
+    'ngResource'
+  ]);
